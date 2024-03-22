38583f6b16b69761b4625cf8cc056fdaf28978b8
Михаил Марин <mmn-83@yandex.ru>
2024-03-22 16:02:32 +0300

    Report error

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 
 from openapi_client import Configuration
 from openapi_client.api.reports_api import ReportsApi
+from openapi_client.api.expenses_api import ExpensesApi
+from openapi_client.api.incomes_api import IncomesApi
+
 from openapi_client.api_client import ApiClient
 
 
@@ -13,14 +16,29 @@ load_dotenv()
 TOKEN: str = os.getenv('TOKEN')
 API_TOKEN: str = os.getenv('API_TOKEN')
 
-ENDPOINT: str = 'http://moneybox.ddns.net/api/v1/report/'
-HEADERS: dict = {'Authorization': f'Token {API_TOKEN}'}
+# ENDPOINT: str = 'http://moneybox.ddns.net/api/v1/report/'
+HEADERS: dict = {'Authorization': f'Token {API_TOKEN}',
+                 'Content-Type': 'application/json'}
+
+report_url = 'http://moneybox.ddns.net'
+
+
+def initialize_expenses_api():
+    config = Configuration()
+    config.access_token = f'Token {API_TOKEN}'
+    config.host = report_url
+    api_client = ApiClient(
+        configuration=config, 
+        header_name='Authorization', 
+        header_value=HEADERS['Authorization'])
+    expenses_api = ExpensesApi(api_client)
+    return expenses_api
 
 
 def initialize_reports_api():
     config = Configuration()
     config.access_token = f'Token {API_TOKEN}'
-    config.host = ENDPOINT
+    config.host = report_url
     api_client = ApiClient(
         configuration=config,
         header_name='Authorization',
@@ -28,3 +46,15 @@ def initialize_reports_api():
     )
     reports_api = ReportsApi(api_client)
     return reports_api
+
+
+def initialize_incomes_api():
+    config = Configuration()
+    config.access_token = f'Token {API_TOKEN}'
+    config.host = report_url
+    api_client = ApiClient(
+        configuration=config, 
+        header_name='Authorization', 
+        header_value=HEADERS['Authorization'])
+    invites_api = IncomesApi(api_client)
+    return invites_api
--- main.py
+++ main.py
@@ -1,21 +1,47 @@
 import telebot
-
+from datetime import datetime
 from http import HTTPStatus
 from telebot import types
 
-from config import TOKEN, initialize_reports_api
+from config import (TOKEN,
+                    initialize_reports_api,
+                    initialize_expenses_api,
+                    initialize_incomes_api)
 
+from openapi_client import ReportsApi
+from openapi_client.models import Expense, Invite
 
 bot = telebot.TeleBot(token=TOKEN)
+
+
+@bot.message_handler(commands=['expense'])
+def get_expense(message):
+
+    api_client = initialize_expenses_api()
+    
+    expense = api_client.expense_list_with_http_info()
+
+    bot.send_message(message.chat.id, f'{expense.raw_data}')
 
 
 @bot.message_handler(commands=['report'])
 def get_report(message):
 
-    reports_api = initialize_reports_api()
-    reports = reports_api.report_html_retrieve_with_http_info()
-    if reports.status_code == 200:
-        bot.send_message(message.chat.id, f'{reports.raw_data}')
+    api_client = initialize_reports_api()
+
+    report = api_client.report_list_with_http_info()
+
+    bot.send_message(message.chat.id, f'{report.raw_data}')
+
+
+@bot.message_handler(commands=['income'])
+def get_income(message):
+
+    api_client = initialize_incomes_api()
+    
+    income = api_client.income_list_with_http_info()
+
+    bot.send_message(message.chat.id, f'{income.raw_data}')
 
 
 if __name__ == '__main__':
